f1c955cefed34d6aa6b7d87f43da5e05a4ce62e5
Michael Brookes <mbircohoakeels@me.com>
2016-04-22 12:52:58 +0100

    Fix in place for custom I2C exception handling

--- ExceptionAid.h
+++ ExceptionAid.h
@@ -2,24 +2,23 @@
 // Created by Michael Brookes on 19/04/2016.
 //
 
-#ifndef BBBI2CDEVICE_EXCEPTIONAID_H
-#define BBBI2CDEVICE_EXCEPTIONAID_H
+#ifndef I2C_EXCEPTION_AID_H
+#define I2C_EXCEPTION_AID_H
 
 #include <exception>
 #include <iostream>
 
-namespace I2CDevice {
+namespace I2C {
     using namespace std;
 
     class I2CSetupException : public exception {
     public:
-        I2CSetupException(const char* errMessage):errMessage_(errMessage){}
+        I2CSetupException( char* errMessage ):errMessage_(errMessage){}
         const char* what() const throw() { return errMessage_; }
 
     private:
-        const char* errMessage_;
+        char* errMessage_;
     };
-
 }
 
-#endif //BBBI2CDEVICE_EXCEPTIONAID_H
+#endif //I2C_EXCEPTION_AID_H
