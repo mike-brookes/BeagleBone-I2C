4cbe96c76644e59535a0c2c2faaee77511fa9785
Michael Brookes <mbircohoakeels@me.com>
2016-04-22 13:02:12 +0100

    Comments in Interface - fix for size_t _BufferSize

--- IDevice.h
+++ IDevice.h
@@ -8,21 +8,37 @@
 #include <cstdio>
 
 namespace IDevice {
-
+    /*
+     /brief Interface class for all devices
+     */
     class IDevice {
-
         public:
             virtual ~IDevice( ){ };
-
         private:
+            /*
+            /fn pure virtual OpenDevice
+            /brief All devices implementing this interface will require this functionality.
+            */
             virtual int OpenDevice( ) = 0;
+
+            /*
+            /fn pure virtual WriteToDevice
+            /brief All devices implementing this interface will require this functionality.
+            */
             virtual int WriteToDevice( size_t _BufferSize ) = 0;
+
+            /*
+            /fn pure virtual ReadDevice
+            /brief All devices implementing this interface will require this functionality.
+            */
             virtual short ReadDevice( size_t _BufferSize ) = 0;
+
+            /*
+            /fn pure virtual ConnectToDevice
+            /brief All devices implementing this interface will require this functionality.
+            */
             virtual int ConnectToDevice( ) = 0;
-
         protected:
-
     };
-
 }
 #endif //IDEVICE_H
