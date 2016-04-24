5b4b50163862a2a88a838cd7197abbb5168479ed
Michael Brookes <mbircohoakeels@me.com>
2016-04-24 20:44:55 +0100

    Add device is now a part of the interface class

--- IDevice.h
+++ IDevice.h
@@ -12,33 +12,39 @@ namespace IDevice {
      /brief Interface class for all devices
      */
     class IDevice {
-        public:
-            virtual ~IDevice( ){ };
-        private:
-            /*
-            /fn pure virtual OpenDevice
-            /brief All devices implementing this interface will require this functionality.
-            */
-            virtual int OpenDevice( ) = 0;
+    public:
+        virtual ~IDevice( ){ };
 
-            /*
-            /fn pure virtual WriteToDevice
-            /brief All devices implementing this interface will require this functionality.
-            */
-            virtual int WriteToDevice( size_t _BufferSize ) = 0;
+        /*
+        /fn pure virtual OpenDevice
+        /brief All devices implementing this interface will require this functionality.
+        */
+        virtual int InitDevice( ) = 0;
 
-            /*
-            /fn pure virtual ReadDevice
-            /brief All devices implementing this interface will require this functionality.
-            */
-            virtual short ReadDevice( size_t _BufferSize ) = 0;
+        /*
+        /fn pure virtual OpenDevice
+        /brief All devices implementing this interface will require this functionality.
+        */
+        virtual int OpenDevice( ) = 0;
 
-            /*
-            /fn pure virtual ConnectToDevice
-            /brief All devices implementing this interface will require this functionality.
-            */
-            virtual int ConnectToDevice( ) = 0;
-        protected:
+        /*
+        /fn pure virtual WriteToDevice
+        /brief All devices implementing this interface will require this functionality.
+        */
+        virtual int WriteToDevice( size_t _BufferSize ) = 0;
+
+        /*
+        /fn pure virtual ReadDevice
+        /brief All devices implementing this interface will require this functionality.
+        */
+        virtual short ReadDevice( size_t _BufferSize ) = 0;
+
+        /*
+        /fn pure virtual ConnectToDevice
+        /brief All devices implementing this interface will require this functionality.
+        */
+        virtual int ConnectToDevice( ) = 0;
+    protected:
     };
 }
 #endif //IDEVICE_H
