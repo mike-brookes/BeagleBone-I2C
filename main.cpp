081e6002aeac5712f59ecbb7ac90606b19b01db6
Michael Brookes <mbircohoakeels@me.com>
2016-04-29 15:28:44 +0100

    Updating project structure

--- main.cpp
+++ main.cpp
@@ -6,22 +6,26 @@
 //  Copyright © 2015 Michael Brookes. All rights reserved.
 //
 
-#include "./Device/I2CDevice.h"
+#include "Lib/I2C/I2CDevice.h"
 
 /*
  * Simplest implementation of a usable class extending an I2C device
  */
-class LSM303_Accelerometer : public I2C::I2CDevice {
+class LSM303DLHC : public I2C::I2CDevice {
+public:
+    LSM303DLHC( ) { }
+    void SetDeviceAddress( unsigned char _DeviceAddress ){ this->DeviceAddress = _DeviceAddress; }
+    void SetBusId( int _BusId ) { this->BusId = _BusId; }
+};
+
+class LSM303_Accelerometer : public LSM303DLHC{
 public:
     LSM303_Accelerometer( unsigned char _DeviceAddress, int _BusId ) {
         this->SetDeviceAddress( _DeviceAddress );
         this->SetBusId( _BusId );
         this->InitDevice( );
     }
-    void SetDeviceAddress( unsigned char _DeviceAddress ){ this->DeviceAddress = _DeviceAddress; }
-    void SetBusId( int _BusId ) { this->BusId = _BusId; }
 };
-
 /*
  * Test I2C functionality
  * Prints a short (8 bits) with current X reading from the accelerometer.
